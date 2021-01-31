8751d8222197423d56ff1298c2bbb9eb783f1c83
Stephen Warren <swarren@wwwdotorg.org>
2021-01-30 20:31:06 -0700

    Day 5 in C++

---
+++ c++/day5.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+auto readParseInput(std::string fileName) {
+    std::ifstream file(fileName);
+    std::vector<int> result;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        int v = 0;
+        for (auto c: line) {
+            v <<= 1;
+            if ((c == 'B') || (c == 'R'))
+                v |= 1;
+        }
+        result.push_back(v);
+    }
+    file.close();
+
+    return result;
+}
+
+int answer(std::vector<int> &input) {
+    sort(input.begin(), input.end());
+    return input.back();
+}
+
+int main(void) {
+    auto input = readParseInput("../input/day5.txt");
+    auto answer_ = answer(input);
+    std::cout << answer_ << '\n';
+    return 0;
+}
